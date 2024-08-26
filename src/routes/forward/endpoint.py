be9b82c73a697bc8ccf75dd12d72140d75c12529
Farnam <bluewhale.f@gmail.com>
2024-08-26 20:54:14 +0330

    add simple cache for check reduce db calls

--- src/routes/forward/endpoint.py
+++ src/routes/forward/endpoint.py
@@ -1,8 +1,8 @@
-from typing import Union
 from fastapi import APIRouter, HTTPException
 from pydantic.types import constr
 from starlette.responses import RedirectResponse
 from src.models.link.models import LinkDbModel
+from src.services.link.helper import get_shorten_link_from_cache,set_shorten_link_in_cache
 
 router_v1 = APIRouter(prefix="", tags=["forward"])
 
@@ -11,8 +11,16 @@ router_v1 = APIRouter(prefix="", tags=["forward"])
 async def redirect_user(
     shorten_path: constr(min_length=5, max_length=5)
 ) -> RedirectResponse:
+    # Check cache first
+    cached_link = await get_shorten_link_from_cache(shorten_path)
+    if cached_link:
+        return RedirectResponse(cached_link["original_link"])
+
+    # Cache miss, query the database
     shorten_link = await LinkDbModel.find_one({"shorten_path": shorten_path})
     if shorten_link:
+        # Save the result in cache
+        await set_shorten_link_in_cache(shorten_path, shorten_link)
         return RedirectResponse(shorten_link.original_link)
     else:
         raise HTTPException(status_code=404, detail="Shorten link not found")
--- src/services/link/helper.py
+++ src/services/link/helper.py
@@ -1,11 +1,23 @@
+import json
+from typing import Union
 from bson.objectid import ObjectId
 from fastapi import HTTPException
 
 from src.models.link.models import LinkDbModel
-
+from src.extensions import redis_cache
+from src.config import settings
 
 async def delete_shorten_link(link_id: str):
     result = LinkDbModel.find_one(LinkDbModel.id == ObjectId(link_id))
     if await result.count() == 0:
         raise HTTPException(status_code=404, detail="Link not found")
     await result.delete()
+
+async def get_shorten_link_from_cache(shorten_path: str) -> Union[dict, None]:
+    cached_data = redis_cache.get(shorten_path)
+    if cached_data:
+        return json.loads(cached_data)
+    return None
+
+async def set_shorten_link_in_cache(shorten_path: str, shorten_link: LinkDbModel):
+    redis_cache.setex(shorten_path, settings.CACHE_TTL, shorten_link.model_dump_json())
